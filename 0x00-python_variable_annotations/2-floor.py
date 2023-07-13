d6c7687889ffaf65fd20eff0a92569e8174f73fe
Sylvia Otieno <sylvia.a.otieno@gmail.com>
2023-07-13 12:28:47 +0300

    Function floor returning floor of a float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+    Function floor which takes a float 'n' as argument
+'''
+
+
+def floor(a: float) -> int:
+    '''
+        Returns the floor of the float
+    '''
+    return int(a)
