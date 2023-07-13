46bcc7ebb81db227ca16a4116d85ccee66467d20
Sylvia Otieno <sylvia.a.otieno@gmail.com>
2023-07-13 09:08:51 +0300

    Function concat returning concatenated string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+    Function concat that takes a string 'str1' and a string 'str2' as arguments
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+        Returns a concatenated string
+    '''
+    return str1 + str2
