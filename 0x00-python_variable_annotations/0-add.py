1643db5b5bd6bfb41615017690bac40ed1e8231e
Sylvia Otieno <sylvia.a.otieno@gmail.com>
2023-07-13 09:01:32 +0300

    Function add returning sum of float numbers

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+'''
+Function add that takes floats a and b as arguments
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''
+    Adds a and b float numbers and returns their sum as a float
+    '''
+    return a + b
